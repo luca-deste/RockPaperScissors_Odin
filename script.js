bf26fdd70b499bea95b840ef93f6554a7feca5b1
Luca <lucadeste21@gmail.com>
2022-05-06 23:23:18 +0200

    Added game function

--- script.js
+++ script.js
@@ -9,16 +9,38 @@ function computerPlay(){
 function playRound(playerSelection,computerSelection){
     playerSelection = playerSelection[0].toUpperCase()+playerSelection.slice(1).toLowerCase()
     if((playerSelection=='Rock' && computerSelection=='Scissors')||(playerSelection=='Scissors' && computerSelection=='Paper')||(playerSelection=='Paper' && computerSelection=='Rock')){
-        let winner = 'You win! '+playerSelection+' beats '+computerSelection;
+        let winner = 'player';
+        console.log(winner)
         return winner;
     } else if(playerSelection == computerSelection){
-        let winner = 'It\'s a Draw!'
+        let winner = 'draw!';
+        console.log(winner);
         return winner;
     } else {
-        let winner = 'You lose! ' + computerSelection + ' beats '+playerSelection;
-        return winner
+        let winner = 'pc';
+        console.log(winner);
+        return winner;
     }
 }
-let playerSelection = 'Rock';
-let computerSelection = computerPlay();
-console.log(playRound(playerSelection,computerSelection));
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i=0;i<5;i++){
+        let playerSelection = prompt('Please, select an option between Rock, Paper and Scissors to play. ');
+        let computerSelection = computerPlay();
+        result = playRound(playerSelection,computerSelection)
+        if(result=='player'){
+            playerScore++;
+        } else if (result=='pc'){
+            computerScore++;
+        } else {
+            playerScore++;
+            computerScore++;
+        }
+        console.log('Player Score: ' +playerScore )
+        console.log('Pc Score: ' + computerScore)
+    }
+    let FinaleScore = computerScore>playerScore ? 'You Lose' : computerScore<playerScore ? 'You Won' : 'It\'s a Draw!'
+    console.log(FinaleScore)
+}
+game()
