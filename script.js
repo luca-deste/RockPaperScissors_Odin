6cd163e6deca35e28db6001808d137544afd1803
Luca <lucadeste21@gmail.com>
2022-05-06 22:36:21 +0200

    Added function computerPlay()

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log('hello world');
+function computerPlay(){
+    let gameChoices = ['Rock','Paper','Scissors'];
+    let randomChoice = gameChoices[Math.floor(Math.random()*gameChoices.length)];
+    console.log(randomChoice)
+    return randomChoice;
+}
+computerPlay()
