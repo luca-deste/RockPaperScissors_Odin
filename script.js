48675d589b4e21e7c02e3f732de6d768b4d27357
Luca <lucadeste21@gmail.com>
2022-05-06 22:58:58 +0200

    added function playRound

--- script.js
+++ script.js
@@ -5,4 +5,20 @@ function computerPlay(){
     console.log(randomChoice)
     return randomChoice;
 }
-computerPlay()
+//computerPlay()
+function playRound(playerSelection,computerSelection){
+    playerSelection = playerSelection[0].toUpperCase()+playerSelection.slice(1).toLowerCase()
+    if((playerSelection=='Rock' && computerSelection=='Scissors')||(playerSelection=='Scissors' && computerSelection=='Paper')||(playerSelection=='Paper' && computerSelection=='Rock')){
+        let winner = 'You win! '+playerSelection+' beats '+computerSelection;
+        return winner;
+    } else if(playerSelection == computerSelection){
+        let winner = 'It\'s a Draw!'
+        return winner;
+    } else {
+        let winner = 'You lose! ' + computerSelection + ' beats '+playerSelection;
+        return winner
+    }
+}
+let playerSelection = 'Rock';
+let computerSelection = computerPlay();
+console.log(playRound(playerSelection,computerSelection));
